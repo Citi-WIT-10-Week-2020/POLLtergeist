69db598e1ae77c6fc1faf43ae14707b7f093c37c
Emily Hannon <georgiannahannon@gmail.com>
2020-07-01 16:34:21 -0400

    initialize java portion of the project

---
+++ POLLtergeistJava/POLLtergeist/src/main/java/com/POLLtergeist/project/PolLtergeistApplication.java
@@ -0,0 +1,13 @@
+package com.POLLtergeist.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolLtergeistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolLtergeistApplication.class, args);
+	}
+
+}
