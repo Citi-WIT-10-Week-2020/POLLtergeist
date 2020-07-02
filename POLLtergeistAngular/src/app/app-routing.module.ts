2cb75a6046d66a592ff4dd71af95c6e6a1ee083a
Emily Hannon <georgiannahannon@gmail.com>
2020-07-02 12:15:42 -0400

    cleanup

--- POLLtergeistAngular/src/app/app-routing.module.ts
+++ POLLtergeistAngular/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-
 
 const routes: Routes = [
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent}
+
 ];
 
 @NgModule({
