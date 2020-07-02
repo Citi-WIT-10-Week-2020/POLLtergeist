a272c38a2efe460f4fa73726ddf00a7fc2fad701
Emily Hannon <georgiannahannon@gmail.com>
2020-07-02 10:47:58 -0400

    initialize login and signup components

--- POLLtergeistAngular/src/app/app.module.ts
+++ POLLtergeistAngular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginSignupHeaderComponent } from './login-signup-header/login-signup-header.component';
+import { SignupLoginComponent } from './signup-login/signup-login.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignupHeaderComponent,
+    SignupLoginComponent,
+    LoginSignupComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
