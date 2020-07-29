4f7094fb984c51c2e22c35d75ccacd78427d0038
mariarea01 <66438113+mariarea01@users.noreply.github.com>
2020-07-29 19:02:23 -0400

    updated pollspage

--- POLLtergeistAngular/src/app/internal/create/createpollspage/pollspage.component.ts
+++ POLLtergeistAngular/src/app/internal/create/createpollspage/pollspage.component.ts
@@ -9,26 +9,78 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 
 })
-export class PollspageComponent implements OnInit {
-  myForm:FormGroup;
-  constructor(private fb: FormBuilder) { }
+export class PollspageComponent {
+  data = {
+    questions: [
+      {
+        question: "",
+        options: [
+          {
+            option: "",
+          }
+        ]
+      }
+    ]
+  }
 
-  ngOnInit(){
-    this.myForm= this.fb.group({
-      option: this.fb.array([])
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      title: [''],
+      questions: this.fb.array([])
     })
 
+    this.setQuestions();
   }
-    get optionForms(){
-      return this.myForm.get('option') as FormArray
-    }
-    addOption(){
-      const option = this.fb.group({
-        answer:[]
+
+  onSubmit() {
+    alert(this.myForm.value);
+  }
+
+  addNewQuestion() {
+    let control = <FormArray>this.myForm.controls.questions;
+    control.push(
+      this.fb.group({
+        question: [''],
+        options: this.fb.array([])
       })
-      this.optionForms.push(option);
-    }
-    deleteOption(i) {
-      this.optionForms.removeAt(i);
-    }
+    )
   }
+
+
+  deleteQuestion(index) {
+    let control = <FormArray>this.myForm.controls.questions;
+    control.removeAt(index)
+  }
+
+  addNewOption(control) {
+    control.push(
+      this.fb.group({
+        option: ['']
+      }))
+  }
+
+  deleteOption(control, index) {
+    control.removeAt(index)
+  }
+
+  setQuestions() {
+    let control = <FormArray>this.myForm.controls.questions;
+    this.data.questions.forEach(x => {
+      control.push(this.fb.group({ 
+        question: x.question, 
+        options: this.setOptions(x) }))
+    })
+  }
+
+  setOptions(x) {
+    let arr = new FormArray([])
+    x.options.forEach(y => {
+      arr.push(this.fb.group({ 
+        option: y.option
+      }))
+    })
+    return arr;
+  }
+}
