a7509b10baf2ee915d15d3063476e52397964c25
Emily Hannon <georgiannahannon@gmail.com>
2020-07-30 12:48:49 -0400

    redo sign up login routing

--- POLLtergeistAngular/src/app/app-routing.module.ts
+++ POLLtergeistAngular/src/app/app-routing.module.ts
@@ -10,6 +10,9 @@ import { SurveysComponent } from './internal/surveys/surveys.component';
 import { CreateSurveyComponent } from './internal/create/create-survey/create-survey.component';
 import { DraftsComponent } from './internal/drafts/drafts.component';
 import { ViewpollComponent } from './internal/viewpoll/viewpoll.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
+import { CreateAccountComponent } from './login/create-account/create-account.component';
 
 
 const routes: Routes = [ {path: '', component: HomepageComponent}, 
@@ -18,11 +21,14 @@ const routes: Routes = [ {path: '', component: HomepageComponent},
 {path: 'business', component:BusinesspackComponent},
 {path: 'aboutus', component:AboutusComponent},
 {path: 'pollspage', component:PollspageComponent},
-{path:"polls", component: PollsComponent},
+{path: "polls", component: PollsComponent},
 {path: "viewpoll", component: ViewpollComponent},
 {path: "surveys", component: SurveysComponent},
-{path:"newSurvey", component: CreateSurveyComponent},
-{path: "drafts", component: DraftsComponent}
+{path: "newSurvey", component: CreateSurveyComponent},
+{path: "drafts", component: DraftsComponent},
+{path: 'login', component: LoginPageComponent},
+{path: 'signup', component: SignUpComponent},
+{path: 'createaccount', component: CreateAccountComponent}
 
 ];
 
