2d141aef77d21cbb0d3caa281ecb6d8bb9c9016c
kxnknxn <67010632+kxnknxn@users.noreply.github.com>
2020-08-03 14:25:32 -0400

    editing drafts component

--- POLLtergeistAngular/src/app/internal/drafts/drafts.component.ts
+++ POLLtergeistAngular/src/app/internal/drafts/drafts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService} from 'src/app/services/header.service' 
+import { InternalheaderService } from 'src/app/services/internalheader.service';
+import { Route, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-drafts',
@@ -7,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DraftsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, public internalNav: InternalheaderService, public nav: HeaderService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    
+    this.nav.hide(); //hides homepage navbar
+    this.internalNav.show(); //shows internal elements navbar 
   }
 
 }
