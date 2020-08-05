1f14141fd606859f595f522890c5b8cf991359d7
ShreyaGazawada <shreya.gazawada@gmail.com>
2020-08-05 15:16:48 -0500

    example update

--- POLLtergeistAngular/src/app/login/login-page/login-page.component.ts
+++ POLLtergeistAngular/src/app/login/login-page/login-page.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
+
 export class LoginPageComponent implements OnInit {
 
   hide: boolean = true; 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
   
   constructor() { }
 
--- POLLtergeistAngular/src/app/services/data.js
+++ POLLtergeistAngular/src/app/services/data.js
@@ -4,19 +4,19 @@ export const DATA = {
     question1: {
       title: "Work Environment",
       questionTitle: "Which work setting do you prefer?",
-      answerChoices: [" Work from home", " Working in the office", " Hybrid"],
+      answerChoices: [" Work from home", " Working in the office", " I prefer not working"],
       index: 1
     },
     question2: {
       title: "Favorite Shows",
       questionTitle: "What's your favorite show?",
-      answerChoices: ["Psych", "IASIP", "New Girl"],
+      answerChoices: [" Psych", " IASIP", " New Girl"],
       index: 2
     },
     question3: {
       title: "Fall Semester",
       questionTitle: "Are you prepared for the fall semester?",
-      answerChoices: ["yes", "no", "I don't know"],
+      answerChoices: [" yes", " no", " I don't know"],
       index: 3
     },
     surveyQuestion: {
